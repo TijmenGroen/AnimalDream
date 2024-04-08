bd4127216b90da7aa625e10b5d0d4b96e61826b1
Tijmen Groen <tijmen.groen@outlookc.com>
2024-04-08 17:20:44 +0200

    removed tailwind from all css

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -6,13 +6,13 @@ function Navigation() {
   return (
     <div className="navbar">
       <div className="navbar-start">
-        <a href="">
+        <a href="/">
           <img src={Logo} alt="logo" style={{height: "5rem", float: "left", marginRight: "1rem"}}/>
           <span>Animal Dream</span>
         </a>
       </div>
       <div className="navbar-center">
-        <a href=""><div className="navbar-icon">Products<Compass size={18}/></div></a>
+        <a href="/products"><div className="navbar-icon">Products<Compass size={18}/></div></a>
         <a href=""><div className="navbar-icon">Cart<ShoppingCart size={18}/></div></a>
         <a href=""><div className="navbar-icon">About<LibraryBig size={18}/></div></a>
       </div>
