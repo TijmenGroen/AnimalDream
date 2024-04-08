8ef5f48a74b53b700094916c6b028dd18678aae8
Tijmen Groen <tijmen.groen@outlookc.com>
2024-04-09 00:48:32 +0200

    changed product container styling

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,13 +1,33 @@
 import React from "react";
+import { Heart, ShoppingCart } from "lucide-react";
+import "../css/product.css";
+import Placeholder from "/assets/img/placeholder.svg";
 
 class Product extends React.Component {
-    render() {
-      return(
-        <div className="productContainer">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt saepe earum quo deleniti voluptas eum quisquam dolores vitae aliquid distinctio. Dolorem, facilis consequuntur iste aspernatur molestiae asperiores error ipsam vel!
+  render() {
+    return (
+      <div className="productContainer">
+        <div className="productBanner">
+          <img src={Placeholder} alt="productBanner" />
         </div>
-      );
-    }
+        <div className="productTitle">
+          Hondenlijn - 8m
+          <hr />
+        </div>
+        <div className="productDescription">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+          excepturi recusandae sed deserunt repudiandae impedit voluptates,
+          nostrum similique quia alias quis voluptate, commodi consequatur!
+        </div>
+        <div className="productActionButtons">
+          <button style={{ width: "70%" }}><ShoppingCart /><p style={{paddingTop: "4px", marginLeft: "4px"}}>Bestel</p></button>
+          <button className="favoriteButton">
+            <Heart />
+          </button>
+        </div>
+      </div>
+    );
   }
+}
 
-  export default Product
+export default Product;
--- src/pages/productPage.tsx
+++ src/pages/productPage.tsx
@@ -1,28 +1,10 @@
-import { Heart } from "lucide-react";
+import Product from "../components/product";
 import "../css/product.css";
-import Placeholder from "/assets/img/placeholder.svg";
 
 function ProductPage() {
   return (
-    <div className="productContainer">
-      <div className="productBanner">
-        <img src={Placeholder} alt="productBanner" />
-      </div>
-      <div className="productTitle">
-        Hondenlijn - 8m
-        <hr />
-      </div>
-      <div className="productDescription">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-        excepturi recusandae sed deserunt repudiandae impedit voluptates,
-        nostrum similique quia alias quis voluptate, commodi consequatur!
-      </div>
-      <div className="productActionButtons">
-        <button style={{ width: "80%" }}>Aan winkelwagen toevoegen</button>
-        <button className="favoriteButton">
-          <Heart />
-        </button>
-      </div>
+    <div className="productPage">
+      <Product /><Product /><Product /><Product /><Product /><Product />
     </div>
   );
 }
