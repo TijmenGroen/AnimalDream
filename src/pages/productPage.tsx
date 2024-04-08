2db510d868eb05e1c8517a67b94c27a0bdf71c45
Tijmen Groen <tijmen.groen@outlookc.com>
2024-04-09 00:09:48 +0200

    added product container

--- src/pages/productPage.tsx
+++ src/pages/productPage.tsx
@@ -1,10 +1,29 @@
+import { Heart } from "lucide-react";
 import "../css/product.css";
+import Placeholder from "/assets/img/placeholder.svg";
 
 function ProductPage() {
   return (
-      <div className="navbar">
-        beep
+    <div className="productContainer">
+      <div className="productBanner">
+        <img src={Placeholder} alt="productBanner" />
       </div>
+      <div className="productTitle">
+        Hondenlijn - 8m
+        <hr />
+      </div>
+      <div className="productDescription">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+        excepturi recusandae sed deserunt repudiandae impedit voluptates,
+        nostrum similique quia alias quis voluptate, commodi consequatur!
+      </div>
+      <div className="productActionButtons">
+        <button style={{ width: "80%" }}>Aan winkelwagen toevoegen</button>
+        <button className="favoriteButton">
+          <Heart />
+        </button>
+      </div>
+    </div>
   );
 }
 
