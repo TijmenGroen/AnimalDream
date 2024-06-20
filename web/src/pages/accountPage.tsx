8756592b226af3cb8296046c5303fae83afc138a
Tijmen Groen <tijmen.groen@outlook.com>
2024-06-20 16:06:41 +0200

    made accountpage navigate to home if not logged in

--- web/src/pages/accountPage.tsx
+++ web/src/pages/accountPage.tsx
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
 import ProfileButtons from "../components/profileButtons";
 import UserDataContainer from "../components/userDataContainer";
 import "../css/account.css";
+import { UserService } from "../services/userService";
+import { NavigateFunction, useNavigate } from "react-router-dom";
 
 function AccountPage(): JSX.Element {
+    const navigate: NavigateFunction = useNavigate();
+
+    useEffect(() => {
+        const userService: UserService = new UserService();
+        const loggedIn: boolean = userService.checkIfLoggedIn()
+        if(!loggedIn) navigate("/")
+    })
+
     return (
         <div className="accountPage">
             <ProfileButtons />
